1d2422ada71241fefbbe1baaaf20edc48c3eecc3
Hajdu-Gergo <hajdu.g.013@gmail.com>
2024-05-06 21:55:39 +0200

    Main window készen van

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -101,6 +101,7 @@ namespace QuickServe
                     else if (rdr[5].ToString() == "2") lbElkeszult.Items.Add(rdr[0].ToString() + " - " + rdr[1].ToString() + " - " + rdr[2].ToString() + " - " + rdr[3].ToString());
                     else if (rdr[5].ToString() == "3") lbKiszallitott.Items.Add(rdr[0].ToString() + " - " + rdr[1].ToString() + " - " + rdr[2].ToString() + " - " + rdr[3].ToString());
                 }
+                conn.Close();
                 return true;
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
@@ -125,9 +126,42 @@ namespace QuickServe
             }
         }
 
-        private void FolyamatbanKijelol(object sender, SelectionChangedEventArgs e)
+        private void Kijelol(object sender, SelectionChangedEventArgs e)
         {
+            if(((ListBox)sender).SelectedItem == null) return;
+            string ideigl=((ListBox)sender).SelectedItem.ToString();
+            int id = int.Parse(ideigl[0].ToString());
+            
+            string parancs = "";
 
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string connect;
+            connect = "server=api.uniassist.hu;uid=QuickServe;pwd=Csütörtök8;database=QuickServe";
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = connect;
+                conn.Open();
+                if (ideigl[5] == '3')
+                {
+                    parancs= $"DELETE FROM Rendeles WHERE v_ID={id}";
+                }
+                else
+                {
+                    parancs = $"UPDATE Rendeles SET v_Allapot=v_Allapot+1 WHERE r_ID={id}";
+                }
+
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(parancs, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show(id.ToString());
+            frissit();
         }
     }
 
